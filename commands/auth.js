9dc7babf7b5b9062102729019cb0de50182ae7dd
Max Tsero <max.tsero@outlook.com>
2020-08-19 16:11:43 +0100

    Add initial pass of authentication workflow.

---
+++ commands/auth.js
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) Max Tsero. All rights reserved.
+ * Licensed under the MIT License.
+ */
+const env = require('env-var')
+const botURL = env.get('BOT_URL')
+module.exports = {
+  name: 'auth',
+  description: 'Provides the ability for the bot to authenticate a character and retrive information on structures.',
+  usage: '',
+  cooldown: 5,
+  /**
+   * Provides a simple reply to confirm operation.
+   * @return {null}
+   */
+  async run (client, message, args) {
+    const memberID = message.member.id
+    console.log(`Member ${message.member.name} (ID: ${message.member.id}) authenticated to StructureBot on Server ${message.guild.name}`)
+    message.reply(`Please use this link ${botURL} and sign in with your EVE Online account.`)
+  }
+}
--- services/evesso.js
+++ services/evesso.js
@@ -27,12 +27,14 @@ const structureBotSSO = new sso.SingleSignOn(
   CALLBACKURI,
   {
     scopes: [
-      ''
-    ]
-  userAgent: 'ATODE/StructureBot'
+      'esi-search.search_structures.v1',
+      'esi-universe.read_structures.v1',
+      'esi-corporations.read_structures.v1',
+      'esi-characters.read_notifications.v1',
+      'esi-skills.read_skills.v1'
+    ],
+    userAgent: 'ATODE/StructureBot'
   }
-)
-
 )
 const ssoClient = new esiClient.ESI({
   provider,
@@ -42,10 +44,45 @@ const ssoClient = new esiClient.ESI({
 const http = new Koa()
 const router = new Router()
 let requestToken = UUID.v4()
-let scopes = {
-
-}
 
 router.get('/', async ctx => {
-  const redirectURI = eveSSO.getRedirectUrl(`${requestToken}`, )
-} )
+  const redirectURI = eveSSO.getRedirectUrl(`${requestToken}`)
+
+  ctx.body = `<a href="${redirectUrl}">Log in using EVE Online</a>`
+})
+
+router.get('/sso', async ctx => {
+  const code = ctx.query.code
+  const { character } = await esiClient.register(code)
+
+  ctx.res.statusCode = 302
+  ctx.res.setHeader('Location', `/welcome/${character.characterId}`)
+})
+
+router.get('/welcome/:characterId', async ctx => {
+  const characterId = Number(ctx.params.characterId)
+  const character = await provider.getCharacter(characterId)
+  const token = await provider.getToken(characterId, 'esi-skills.read_skills.v1')
+
+  let body = `<p class="margin: 10px; font-size: 18px;">Welcome, ${character.characterName}!</p>`
+
+  const response = await esi.request(
+    `/characters/${characterId}/skills/`,
+    null,
+    null,
+    { token }
+  )
+
+  const skills = await response.json()
+
+  body += `<p>You have ${skills.total_sp} total skill points.</p><ul>`
+
+  for (const skill of skills.skills) {
+    body += `<li>${skill.skill_id}: ${skill.active_skill_level}</li>`
+  }
+
+  body += '</ul>'
+
+  ctx.body = body
+  
+})
